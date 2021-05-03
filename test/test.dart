e02630b646b687f442560a6cdea4cd5f3129e48b
Robert Ancell <robert.ancell@canonical.com>
2021-05-03 16:53:51 +1200

    Add misc new functions

--- test/test.dart
+++ test/test.dart
@@ -16,8 +16,20 @@ class MockAvahiRoot extends DBusObject {
     }
 
     switch (methodCall.name) {
+      case 'GetAlternativeHostName':
+        var name = (methodCall.values[0] as DBusString).value;
+        return DBusMethodSuccessResponse([DBusString('$name-2')]);
+      case 'GetAlternativeServiceName':
+        var name = (methodCall.values[0] as DBusString).value;
+        return DBusMethodSuccessResponse([DBusString('$name #2')]);
+      case 'GetAPIVersion':
+        return DBusMethodSuccessResponse([DBusUint32(server.apiVersion)]);
+      case 'GetDomainName':
+        return DBusMethodSuccessResponse([DBusString(server.domainName)]);
       case 'GetHostName':
         return DBusMethodSuccessResponse([DBusString(server.hostName)]);
+      case 'GetHostNameFqdn':
+        return DBusMethodSuccessResponse([DBusString(server.hostNameFqdn)]);
       case 'GetVersionString':
         return DBusMethodSuccessResponse([DBusString(server.versionString)]);
       case 'SetHostName':
@@ -32,11 +44,18 @@ class MockAvahiRoot extends DBusObject {
 class MockAvahiServer extends DBusClient {
   late final MockAvahiRoot _root;
 
+  int apiVersion;
+  String domainName;
   String hostName;
+  String hostNameFqdn;
   final String versionString;
 
   MockAvahiServer(DBusAddress clientAddress,
-      {this.hostName = '', this.versionString = ''})
+      {this.apiVersion = 0,
+      this.domainName = '',
+      this.hostName = '',
+      this.hostNameFqdn = '',
+      this.versionString = ''})
       : super(clientAddress);
 
   Future<void> start() async {
@@ -59,6 +78,22 @@ void main() {
     await client.connect();
 
     expect(await client.getVersionString(), equals('1.2.3'));
+
+    await client.close();
+  });
+
+  test('api version', () async {
+    var server = DBusServer();
+    var clientAddress =
+        await server.listenAddress(DBusAddress.unix(dir: Directory.systemTemp));
+
+    var avahi = MockAvahiServer(clientAddress, apiVersion: 512);
+    await avahi.start();
+
+    var client = AvahiClient(bus: DBusClient(clientAddress));
+    await client.connect();
+
+    expect(await client.getAPIVersion(), equals(512));
 
     await client.close();
   });
@@ -92,6 +127,70 @@ void main() {
 
     await client.setHostName('bar');
     expect(avahi.hostName, equals('bar'));
+
+    await client.close();
+  });
+
+  test('get domain name', () async {
+    var server = DBusServer();
+    var clientAddress =
+        await server.listenAddress(DBusAddress.unix(dir: Directory.systemTemp));
+
+    var avahi = MockAvahiServer(clientAddress, domainName: 'local');
+    await avahi.start();
+
+    var client = AvahiClient(bus: DBusClient(clientAddress));
+    await client.connect();
+
+    expect(await client.getDomainName(), equals('local'));
+
+    await client.close();
+  });
+
+  test('get hostname fqdn', () async {
+    var server = DBusServer();
+    var clientAddress =
+        await server.listenAddress(DBusAddress.unix(dir: Directory.systemTemp));
+
+    var avahi = MockAvahiServer(clientAddress, hostNameFqdn: 'foo.local');
+    await avahi.start();
+
+    var client = AvahiClient(bus: DBusClient(clientAddress));
+    await client.connect();
+
+    expect(await client.getHostNameFqdn(), equals('foo.local'));
+
+    await client.close();
+  });
+
+  test('get alternative hostname', () async {
+    var server = DBusServer();
+    var clientAddress =
+        await server.listenAddress(DBusAddress.unix(dir: Directory.systemTemp));
+
+    var avahi = MockAvahiServer(clientAddress);
+    await avahi.start();
+
+    var client = AvahiClient(bus: DBusClient(clientAddress));
+    await client.connect();
+
+    expect(await client.getAlternativeHostName('foo'), equals('foo-2'));
+
+    await client.close();
+  });
+
+  test('get alternative service name', () async {
+    var server = DBusServer();
+    var clientAddress =
+        await server.listenAddress(DBusAddress.unix(dir: Directory.systemTemp));
+
+    var avahi = MockAvahiServer(clientAddress);
+    await avahi.start();
+
+    var client = AvahiClient(bus: DBusClient(clientAddress));
+    await client.connect();
+
+    expect(await client.getAlternativeServiceName('foo'), equals('foo #2'));
 
     await client.close();
   });
